d68532e8e478345fb2b49bf0fb8f4fc8d63b42bc
Jonbosss <jonathanfarhat@gmail.com>
2025-11-19 12:18:57 -0800

    Exercise Complete

--- entry_height.py
+++ entry_height.py
@@ -8,10 +8,11 @@ root.geometry('400x400')
 def myClick():
     hello = 'Hello ' + e.get()
     myLabel = Label(root, text=hello)
+    e.delete(0, END)
     myLabel.pack(pady=10)
 
 
-e = Entry(root, width=50)
+e = Entry(root, width=50, font=('Helvetica', 14))
 e.pack(padx=10, pady=10)
 
 myButton = Button(root, text='Enter Your Name', command=myClick)
